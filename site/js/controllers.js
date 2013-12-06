fafd26752bff48e7d1c0a254be1c0eee5a1b81f6
Michael Little <bio595@gmail.com>
2013-12-06 16:35:53 +1300

    got a splash screen of sorts going

---
+++ site/js/controllers.js
@@ -0,0 +1,21 @@
+var waasApp = angular.module('waasApp', []);
+ 
+waasApp.controller('WallpaperSplashController', function ($scope) {
+  $scope.wallpapers = [
+    {'source': 'images/wallpaper1.jpg'},
+    {'source': 'images/wallpaper2.jpg'},
+    {'source': 'images/wallpaper3.jpg'}
+  ];
+
+  $scope.getStyle = function(wallpaper) {
+  	var dist = 20 * $scope.wallpapers.indexOf(wallpaper);
+  	return "top: " + dist + "px; left: " + dist + "px;";
+  };
+
+  $scope.features = [
+	  {text: "Accounts, that let you have consistent wallpapers across multuiple machines"},
+	  {text: "Wallpapers playlists"},
+	  {text: "Wallpaper browsing and discovery"},
+	  {text: "Potentially users can \"sell\" wallpapers they've created"}
+  ];            
+});
