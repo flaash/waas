2f028a02ed329f5f7aa45346f8c74b737ca2bd83
Michael Little <bio595@gmail.com>
2013-12-06 20:58:59 +1300

    prettified controllers.js and added a timeout to cycle the wallpapers

--- site/js/controllers.js
+++ site/js/controllers.js
@@ -1,21 +1,34 @@
 var waasApp = angular.module('waasApp', []);
- 
-waasApp.controller('WallpaperSplashController', function ($scope) {
-  $scope.wallpapers = [
-    {'source': 'images/wallpaper1.jpg'},
-    {'source': 'images/wallpaper2.jpg'},
-    {'source': 'images/wallpaper3.jpg'}
-  ];
 
-  $scope.getStyle = function(wallpaper) {
-  	var dist = 20 * $scope.wallpapers.indexOf(wallpaper);
-  	return "top: " + dist + "px; left: " + dist + "px;";
-  };
+waasApp.controller('WallpaperSplashController', function($scope, $timeout) {
+    $scope.wallpapers = [{
+        'source': 'images/wallpaper1.jpg'
+    }, {
+        'source': 'images/wallpaper2.jpg'
+    }, {
+        'source': 'images/wallpaper3.jpg'
+    }];
 
-  $scope.features = [
-	  {text: "Accounts, that let you have consistent wallpapers across multuiple machines"},
-	  {text: "Wallpapers playlists"},
-	  {text: "Wallpaper browsing and discovery"},
-	  {text: "Potentially users can \"sell\" wallpapers they've created"}
-  ];            
+    $scope.getStyle = function(wallpaper) {
+        var dist = 20 * $scope.wallpapers.indexOf(wallpaper);
+        return "top: " + dist + "px; left: " + dist + "px;";
+    };
+
+    $scope.features = [{
+        text: "Accounts, that let you have consistent wallpapers across multuiple machines"
+    }, {
+        text: "Wallpapers playlists"
+    }, {
+        text: "Wallpaper browsing and discovery"
+    }, {
+        text: "Potentially users can \"sell\" wallpapers they've created"
+    }];
+
+    function cycle() {
+        $timeout(function() {
+            $scope.wallpapers.unshift($scope.wallpapers.pop());
+            cycle();
+        }, 5000);
+    }
+    cycle();
 });
