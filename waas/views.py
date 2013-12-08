2a653eca7a7ebfb1800da32c0a53f32731e295ed
Michael Little <bio595@gmail.com>
2013-12-08 18:00:09 +1300

    created a layout for templates to extend from

--- waas/views.py
+++ waas/views.py
@@ -6,3 +6,7 @@ from waas import app
 @app.route("/")
 def index():
     return render_template('splash.html')
+
+@app.route("/dashboard")
+def dashboard():
+	return render_template("dashboard.html")
